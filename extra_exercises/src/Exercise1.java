5270d0fae600d3e468a6f0840dd0d5d9e5e08da5
alicjad <alicjad14@gmail.com>
2017-10-16 23:04:09 +0200

    Selection sort function added for arrays

--- extra_exercises/src/Exercise1.java
+++ extra_exercises/src/Exercise1.java
@@ -1,18 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Exercise1 {
     static long startTime;
 
     public static void main(String[]args){
-        for (int j=0; j<100; j++) {
-            int[] optionsArray = {1000, 5500, 10000, 55000, 100000, 550000, 1000000, 5500000, 10000000};
+        for (int j=0; j<1; j++) {
+            int[] optionsArray = {15};
             for (int i = 0; i < optionsArray.length; i++) {
                 System.out.print(optionsArray[i]);
                 int size = optionsArray[i];
                 int[] array = new int[size];
                 fillOutArray(array);
-                int biggestNumber = getBiggestElement(array);
-                System.out.print(biggestNumber);
+                printArray(array);
+                printArray(selectionSort(array));
+                System.out.print(" " + stopTimer() + " ");
                 System.out.println();
             }
         }
@@ -38,16 +41,30 @@ public class Exercise1 {
         }
     }
 
-    public static int getBiggestElement(int[] chosenArray){
-        startTimer();
-        int biggestElement = chosenArray[0];
-        for (int i=0; i<chosenArray.length;i++){
-            if (chosenArray[i]>biggestElement){
-                biggestElement=chosenArray[i];
+    public static int getIndexOfMaxElement(int[] chosenArray, int upperIndexLimit){
+        int maxElement = chosenArray[0];
+        int maxIndex = 0;
+        for (int i=0; i<upperIndexLimit;i++){
+            if (chosenArray[i]>maxElement){
+                maxElement=chosenArray[i];
+                maxIndex = i;
             }
         }
-        System.out.print(" " + stopTimer() + " ");
-        return biggestElement;
+        return maxIndex;
+    }
+
+    public static int[] selectionSort(int[] arrayToSort){
+        for (int i=0; i<arrayToSort.length; i++){
+            int indexOfMaxElement = getIndexOfMaxElement(arrayToSort, arrayToSort.length - i);
+            swapElements(arrayToSort, arrayToSort.length - i-1, indexOfMaxElement);
+        }
+        return arrayToSort;
+    }
+
+    private static void swapElements(int[] arrayToSort, int i, int indexOfMaxElement) {
+        int temp = arrayToSort[i];
+        arrayToSort[i] = arrayToSort[indexOfMaxElement];
+        arrayToSort[indexOfMaxElement] = temp;
     }
 
     public static long startTimer(){
