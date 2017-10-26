8284f95d38c03ac9372705d01c16d63ecba6d6e4
alicjad <alicjad14@gmail.com>
2017-10-26 14:51:24 +0200

    adding new created classes to git track files

---
+++ Mandatory2/src/Buildings/RentOffice.java
@@ -0,0 +1,38 @@
+package Buildings;
+
+import Game.GameObject;
+import Game.State;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentOffice extends GameObject {
+    String name;
+    GameObject parent;
+
+    public void execute(State state)
+    {
+        state.setSteps(5);
+    }
+
+    public RentOffice (String name, GameObject parent)
+    {
+        this.name = name;
+        this.parent = parent;
+    }
+
+    public List<GameObject> getAccessibleObjects()
+    {
+        List<GameObject> list = new ArrayList<GameObject>();
+        list.add(parent);
+        return list;
+    }
+
+    public String getWelcomeMessage()
+    {
+        return "Welcome to the "+name+ ".";
+    }
+    public String getOptionMessage(){
+        return "Go to "+name;
+    }
+}
